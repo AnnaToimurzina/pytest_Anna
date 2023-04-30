cd6b961e91c7e4ce5cb1034a572edabd5d3bb69b
AnnaToimurzina <2402as@gmail,com>
2023-05-01 00:20:40 +0500

    добавлены тесты коммитов2

--- tests/test_arrs.py
+++ tests/test_arrs.py
@@ -9,3 +9,5 @@ def test_get():
 def test_my_slice():
     assert arrs.my_slice([1, 2, 3, 4, 5], 1, 3) == [2, 3]
     assert arrs.my_slice([1, 2, 3, 4, 5], 2, 4) == [3, 4]
+    assert arrs.my_slice([1, 2, 3, 4, 5], 2, 4) == [3, 4]
+
